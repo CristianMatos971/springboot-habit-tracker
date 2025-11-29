cb9c0aa19f7d212a28765c0adeaf9e95fc038550
CristianMatos <cristianernandes971@gmail.com>
2025-11-29 16:09:44 -0400

    Add allowed Origins for the API

--- habittracker-backend/habittracker/src/main/java/com/example/habittracker/security/config/SecurityConfig.java
+++ habittracker-backend/habittracker/src/main/java/com/example/habittracker/security/config/SecurityConfig.java
@@ -43,7 +43,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "springboot-habit-tracker.vercel.app",
+                "springboot-habit-tracker-git-main-cristian-matos-projects.vercel.app",
+                "springboot-habit-tracker-f3jux4f5z-cristian-matos-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
